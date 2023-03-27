6b635e6e677bd3ee41719435481eea8f14f5f4b4
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2023-03-27 14:13:51 -0500

    Se carga conexion de base de datos de archivo de configuracion.

--- infraestructure/database/mysql.go
+++ infraestructure/database/mysql.go
@@ -2,12 +2,36 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
 func NewMySQLDB() (*sql.DB,error) {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hexagonal")
+
+	viper.SetConfigFile("./infraestructure/database/config/config.yml")
+	if err := viper.ReadInConfig(); err != nil {
+        panic(fmt.Errorf("error al leer archivo de configuración: %s", err))
+    }
+	// Obtén los valores de configuración
+    host := viper.GetString("database.mysql.host")
+    port := viper.GetInt("database.mysql.port")
+    user := viper.GetString("database.mysql.user")
+    password := viper.GetString("database.mysql.password")
+    dbname := viper.GetString("database.mysql.dbname")	
+	
+	if os.Getenv("MYSQL_PASSWORD")!= "" {
+		fmt.Println("variable de entorno Password: " +os.Getenv("MYSQL_PASSWORD"))
+		password = os.Getenv("MYSQL_PASSWORD")
+	}
+	
+	fmt.Println("Password: " + password)
+	// Usa los valores de configuración para conectarte a MySQL
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, port, dbname)
+
+	db, err := sql.Open("mysql",dsn)
 	if err != nil {
 		return nil, err
 	}
